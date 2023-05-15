7e6f2a3385fb02a92da1a58d30f24d5eb1550b43
Raqib Hayder <1992290+raqib-hayder@users.noreply.github.com>
2023-05-15 14:44:34 -0400

    Setup application skeleton

---
+++ services/tasks_api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+@app.get("/health-check")
+async def health_check():
+    return {"message": "ok"}
---
+++ services/tasks_api/tests.py
@@ -0,0 +1,21 @@
+import pytest
+from fastapi import status
+from starlette.testclient import TestClient
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health_check(client: TestClient):
+    """
+    GIVEN a FastAPI application
+    WHEN health check endpoint is called with GET method
+    THEN respond with status 200 and body OK message
+    """
+    response = client.get("/health-check")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "ok"}
